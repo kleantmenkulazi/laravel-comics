6442ae4bb9ab572df616a89e5f54072c7aa30a7c
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-10-23 01:36:22 +0200

    aggiornata scrittura ma mi da comunque errore

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function ()) {
+Route::get('/', function () {
+    $comics= config('comics');
     return view('home, ['comics' => $comis]);
 });
 
